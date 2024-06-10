ea805e2b6a063f77280f6d58df5ce13aab367719
Nafy92 <musanefeesahoj@gmail.com>
2024-06-10 15:20:31 +0100

    added an attribute to the JS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
   console.log(reponse.data);
