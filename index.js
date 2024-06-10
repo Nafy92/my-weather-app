535fa91d8730cb29201df1235872b3cdf192cc9d
Nafy92 <musanefeesahoj@gmail.com>
2024-06-10 21:21:23 +0100

    changed the content of the html and JS file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function refreshWeather(response) {
-  console.log("weather data received:", response.data);
+  console.log("Weather data received:", response.data);
 
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
@@ -8,19 +8,15 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
+  let date = new Date(response.data.time * 1000);
 
-  iconElement.innerHTML = `<img
-      src="${response.data.condition.icon_url}" alt="Weather icon"
-      class="weather-app-temperature-icon"
-    />
-  `;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" alt="Weather icon" class="weather-app-temperature-icon"/>`;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   descriptionElement.innerHTML = response.data.condition.description;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   timeElement.innerHTML = formatDate(date);
 }
 
@@ -50,27 +46,27 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
 function searchCity(city) {
   let apiKey = "208923ot246cf9a44e16fa303a8c757b";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-   console.log("Fetching weather data from:", apiUrl);
+  console.log("Fetching weather data from:", apiUrl);
   axios.get(apiUrl).then(refreshWeather).catch(handleError);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-}
-  console.log("Form submitted");
 
   let searchInput = document.querySelector("#search-form-input");
   if (searchInput) {
     let searchValue = searchInput.value.trim();
-    if (searchValue){
-        searchCity(searchValue);
-    }else{
-        let cityElement=document.querySelector("#city")
-         cityElement.innerHTML = "Please enter a city name.";
+    if (searchValue) {
+      searchCity(searchValue);
     } else {
+      let cityElement = document.querySelector("#city");
+      cityElement.innerHTML = "Please enter a city name.";
+    }
+  } else {
     console.error("Search input element is missing in the HTML.");
   }
 }
