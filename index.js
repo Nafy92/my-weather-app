b5d7f0dc44e82ed888ed75e600bb6d9a8839199b
Nafy92 <musanefeesahoj@gmail.com>
2024-06-10 13:04:19 +0100

    changed the content of the JS

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
+
+  console.log("Form submitted");
+
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  if (searchInput && cityElement) {
+    let searchValue = searchInput.value.trim();
+    console.log("Search value:", searchValue);
+    if (searchValue) {
+      cityElement.innerHTML = searchValue;
+    } else {
+      cityElement.innerHTML = "Please enter a city name.";
+    }
+  } else {
+    console.error("Required elements are missing in the HTML.");
+  }
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+if (searchFormElement) {
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  console.log("Event listener added to form");
+} else {
+  console.error("Search form element is missing in the HTML.");
+}
